0d219f24e7c6fd9660ed4fd3bfc76e4ce9d4b08f
Jake Herman <jakeherman3@gmail.com>
2014-03-21 13:10:15 -0400

    cleanup some stuff

--- automatic.sh
+++ automatic.sh
@@ -9,22 +9,23 @@ if ! ([ $# == 1 ] && [ $1 == "--auto" ]); then
 else
     cd /home/jake/final-project/
     source /usr/local/rvm/environments/ruby-2.0.0-p451@rails
-    rake db:reset
-    cp db/fresh-preSchedules.yml db/data.yml
-    rake db:data:load
-    DISPLAY=:77 rake schedules:parseSchedules
-    rake schedules:purgeBlankSchedules
-    rake schedules:convertToCommitments
+    #rake db:reset
+    #cp db/fresh-preSchedules.yml db/data.yml
+    #rake db:data:load
+    #DISPLAY=:77 rake schedules:parseSchedules
+    #rake schedules:purgeBlankSchedules
+    #rake schedules:convertToCommitments
     # Dump the database. -T users excludes the users table
     pg_dump -Fc --no-acl --no-owner -h localhost -U jake -T users final_project_development > /tmp/data.dump
+    cp /tmp/data.dump /srv/http/
     # ;)
     now=$(date +"%m-%d-%Y--%H:%M:%S")
-    filename="data-$now.tar.gz"
-    tar czvf "/home/jake/scheduleArchive/$filename" /tmp/data.dump
+    filename="data-$now.gz"
+    gzip /tmp/data.dump
+    mv /tmp/data.dump.gz "/home/jake/scheduleArchive/$filename"
     # Eww Eww Eww Eww Eww
     # cat data.tar.gz | nc -lcp 1357 & heroku run 'nc jherman.no-ip.org 1357 > /app/data.tar.gz'
     # I'm not sure if this is even more disgusting than the first one...
-    cp /tmp/data.dump /srv/http/
     sudo systemctl start httpd
     heroku pgbackups:restore DATABASE_URL 'http://jherman.no-ip.org/data.dump' --confirm=paschedules
     sudo systemctl stop httpd
