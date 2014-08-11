52b682d2b7905bcfdbffbca85e7571c23aed74d3
Jake Herman <jakeherman3@gmail.com>
2014-08-11 17:10:16 -0400

    Fix #1 by removing before filter

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -1,19 +1,10 @@
 class CommitmentsController < ApplicationController
-  before_filter :check_student, only: :show
-
   def show
     @commitment = Commitment.find(params[:id])
     @students = @commitment.students.page(params[:page]).per(20)
     respond_to do |format|
       format.html
       format.js { render partial: 'ajax_show.js.erb', locals: { section: @commitment } } # Ajax
-    end
-  end
-
-  def check_student
-    stu = Student.find_by(email: current_user.email)
-    if !Commitment.find(params[:id]).students.include? stu
-      redirect_to stu
     end
   end
 end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,5 +1,4 @@
 class SectionsController < ApplicationController
-  # before_filter :check_student, only: :show
   def show
     @section = Section.find(params[:id])
     @students = Kaminari.paginate_array(@section.students.sort { |a, b| a.last_name.downcase <=> b.last_name.downcase }).page(params[:page]).per(20)
@@ -11,11 +10,4 @@ class SectionsController < ApplicationController
 
   def index
   end
-
-  # def check_student
-  #   stu = Student.find_by(email: current_user.email)
-  #   if !Section.find(params[:id]).students.include? stu
-  #     redirect_to stu
-  #   end
-  # end
 end
