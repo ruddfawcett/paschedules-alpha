bd52b618d3a86ece8a9ee6ff926ffb54bb1360d3
Jamie Bloxham <jamie.a.bloxham@gmail.com>
2014-08-19 15:31:28 -0400

    Added veeeeeeeeery minimal logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user! unless Rails.env.test?
+  before_filter :log_data
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def log_data
+    if user_signed_in?
+      logger.info "User #{current_user.email} requesting #{request.env['PATH_INFO']}."
+    else
+      logger.info "Unknown user requesting #{request.env['PATH_INFO']}."
+    end
+  end
 
 end
