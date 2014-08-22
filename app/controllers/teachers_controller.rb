5e31b24ae2ea707c09f7db0ba648f837623fa69d
Jake Herman <jakeherman3@gmail.com>
2014-08-22 17:00:18 -0400

    Preload sections to reduce amount of DB queries

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -13,9 +13,6 @@ class TeachersController < ApplicationController
 
   def show
     @teacher = Teacher.find(params[:id])
-    # I'm prety sure there's a way to do this with SQL so the database does the heavy lifting
-    # This will result in a lot of queries for sections
-    # For our tiny DB size though, it really doesn't matter
-    @sections = @teacher.courses.map(&:sections).flatten.sort_by(&:name)
+    @sections = @teacher.courses.includes(:sections).map(&:sections).flatten.sort_by(&:name)
   end
 end
