3539646dfad6e30ccd887e09daa99045328e0558
Jake Herman <jakeherman3@gmail.com>
2014-08-22 16:49:31 -0400

    Minor cleanup

--- app/controllers/supercourses_controller.rb
+++ app/controllers/supercourses_controller.rb
@@ -1,11 +1,7 @@
 class SupercoursesController < ApplicationController
   def show
     @course = Supercourse.find(params[:id])
-    @courses = @course.courses.sort { |a, b| a.teacher.last_name <=> b.teacher.last_name }
-    @sections = Hash.new()
-    @courses.each do |c|
-      @sections[c] = c.sections.sort { |a, b| a.name <=> b.name }
-    end
+    @courses = @course.courses.sort_by { |x| x.teacher.last_name }
     # if @course.courses.count == 1
     #   redirect_to @course.courses.first
     # end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -13,12 +13,9 @@ class TeachersController < ApplicationController
 
   def show
     @teacher = Teacher.find(params[:id])
-    @sections = Array.new(0)
-    @teacher.courses.each do |c|
-      c.sections.each do |s|
-        @sections << s
-      end
-    end
-    @sections.sort! { |a, b| a.name <=> b.name }
+    # I'm prety sure there's a way to do this with SQL so the database does the heavy lifting
+    # This will result in a lot of queries for sections
+    # For our tiny DB size though, it really doesn't matter
+    @sections = @teacher.courses.map(&:sections).flatten.sort_by(&:name)
   end
 end
