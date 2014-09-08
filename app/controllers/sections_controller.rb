762f8987feb018c46bb03a7b2f82085fb47aa8ab
Jake Herman <jakeherman3@gmail.com>
2014-09-08 11:42:17 -0400

    Remove warning because schedules are out now

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,6 +1,5 @@
 class SectionsController < ApplicationController
   def show
-    flash.now[:warning] = "All class lists are incomplete until the 364 new students have finalized their courses and have had their schedules released by the registrar."
     @section = Section.find(params[:id])
     @students = Kaminari.paginate_array(@section.students.sort_by { |a| a.last_name.downcase }).page(params[:page]).per(@section.students.count > 20 ? 15 : 20)
     respond_to do |format|
