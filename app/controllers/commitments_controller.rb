fdb8fbefdee4d028f82361bea94b517a4a47489a
Jake Herman <jakeherman3@gmail.com>
2014-11-17 10:40:07 -0500

    Fix bug with commitment rendering not being sorted

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -1,7 +1,8 @@
 class CommitmentsController < ApplicationController
   def show
     @commitment = Commitment.find(params[:id])
-    @students = @commitment.students.page(params[:page]).per(@commitment.students.count > 20 ? 15 : 20)
+    @students = Kaminari.paginate_array(@commitment.students.sort_by { |a| a.last_name.downcase }).page(params[:page]).per(@commitment.students.count > 20 ? 15 : 20)
+
     respond_to do |format|
       format.html
       format.js { render partial: 'ajax_show.js.erb', locals: { section: @commitment } } # Ajax
