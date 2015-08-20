d71fda040accff8b726efdd91b971a3fe57e2e13
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-08-20 14:20:52 -0400

    Fixed automatic.sh

--- automatic.sh
+++ automatic.sh
@@ -5,19 +5,17 @@ if ! ([ $# == 1 ] && [ $1 == "--auto" ]); then
     echo "If you still want to run it, run it with the flag --auto"
     exit
 else
-    cd /home/jake/final-project/
-    source /usr/local/rvm/environments/ruby-2.1.0@rails
+    cd ~/GitHub/paschedules
     rake db:reset
-    ruby scheduleRestore.rb https://paschedules-archive.s3.amazonaws.com/base_ids-09_10_2014
-    xpra start :77
-    DISPLAY=:77 rake schedules:parseSchedules &>> log/parser_stdout_stderr.log
-    rake schedules:purgeBlankSchedules &>> log/parser_stdout_stderr.log
-    rake schedules:convertToCommitments &>> log/parser_stdout_stderr.log
-    now=$(date +"%m_%d_%Y-%H_%M_%S")
-    filename="schedules-$now"
-    ruby scheduleDump.rb "/home/jake/scheduleArchive/$filename"
-    aws s3 cp "/home/jake/scheduleArchive/$filename" s3://paschedules-archive/
+    ruby scheduleRestore.rb https://paschedules-archives.s3.amazonaws.com/base_ids-08_19_2015.zip
+    rake schedules:parseSchedules &> log/parser_stdout_stderr.log
+    rake schedules:purgeBlankSchedules &> log/parser_stdout_stderr.log
+    rake schedules:convertToCommitments &> log/parser_stdout_stderr.log
+    now = $(date +"%m_%d_%Y-%H_%M_%S")
+    filename = "schedules-$now"
+    ruby scheduleDump.rb "~/Desktop/schedule-archive/$filename"
+    aws s3 cp "~/Desktop/schedule-archive/$filename" s3://paschedules-archives/
     sleep 10   # S3 takes a little while to process
     echo "Restoring to heroku at $now" >> log/heroku_restore.log
-    /usr/local/heroku/bin/heroku run --app paschedules ruby scheduleRestore.rb "https://paschedules-archive.s3.amazonaws.com/$filename" &>> log/heroku_restore.log
+    /usr/local/heroku/bin/heroku run --app paschedules ruby scheduleRestore.rb "https://paschedules-archives.s3.amazonaws.com/$filename" &> log/heroku_restore.log
 fi
