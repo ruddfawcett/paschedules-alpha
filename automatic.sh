d5e34b16c4bcafdfb3fe069417f3cfd8e5a06f20
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-08-29 23:22:26 -0400

    Fixed automatic.sh

--- automatic.sh
+++ automatic.sh
@@ -1,21 +1,37 @@
-#!/bin/bash -e
+#!/usr/bin/env ruby
 
-if ! ([ $# == 1 ] && [ $1 == "--auto" ]); then
-    echo "WARNING: If you aren't running this on Jake's server, you probably want to edit this file to get rid of stuff specific to that computer."
-    echo "If you still want to run it, run it with the flag --auto"
+#TODO: Comment function of each line.
+
+if ! (([ $# == 1 ] || [ $# == 2 ]) && [ $1 == "--auto" ]); then
+    echo "WARNING: If you aren't running this on Rudd's desktop, you probably want to edit this file to get rid of stuff specific to that computer."
+    echo "If you still want to run it, run it with the flag --auto."
     exit
 else
     cd ~/GitHub/paschedules
+    
     rake db:reset
+    
     ruby scheduleRestore.rb https://paschedules-archives.s3.amazonaws.com/base_ids-08-19-2015.zip
-    rake schedules:parseSchedules &> log/parser_stdout_stderr.log
-    rake schedules:purgeBlankSchedules &> log/parser_stdout_stderr.log
-    rake schedules:convertToCommitments &> log/parser_stdout_stderr.log
-    now = $(date +"%m_%d_%Y-%H_%M_%S")
-    filename = "schedules-$now"
-    ruby scheduleDump.rb "~/Desktop/schedule-archive/$filename"
-    aws s3 cp "~/Desktop/schedule-archive/$filename" s3://paschedules-archives/
-    sleep 10   # S3 takes a little while to process
-    echo "Restoring to heroku at $now" >> log/heroku_restore.log
-    heroku run --app paschedules ruby scheduleRestore.rb "https://paschedules-archives.s3.amazonaws.com/$filename" &> log/heroku_restore.log
+    
+    if [ $2 == "--add-exceptions" ]
+        then
+            rake schedules:addExceptions
+    else
+        echo "This will fail -- if you don't have the --add-exceptions flag (after --auto) and teachers are not in the database."
+    fi
+    
+    rake schedules:parseSchedules > log/parser_automatic.log
+    rake schedules:purgeBlankSchedules >> log/parser_automatic.log
+    rake schedules:convertToCommitments >> log/parser_automatic.log
+    
+	now=$(date +"%m-%d-%Y") #-%H_%M_%S
+	filename="schedules-$now.zip"
+	
+	bundle exec ruby scheduleDump.rb ~/Desktop/schedule-archive/$filename
+	
+	aws s3 cp ~/Desktop/schedule-archive/$filename s3://paschedules-archives/ >> log/aws_upload.log
+	sleep 10   # S3 takes a little while to process
+	
+	echo "Restoring to Heroku on $(date)." >> log/heroku_restore.log
+	heroku run --app paschedules ruby scheduleRestore.rb "https://paschedules-archives.s3.amazonaws.com/$filename" >> log/heroku_restore.log
 fi
